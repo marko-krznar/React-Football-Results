a529f8e39ac58f07f17178aec5b0ecf1176e71ca
Marko-batman <krznarm26@gmail.com>
2021-08-05 16:14:11 +0200

    Set back to initial state

--- src/components/ListClubs.js
+++ src/components/ListClubs.js
@@ -9,19 +9,37 @@ export default class ListClubs extends Component {
         }
     }
 
-    componentDidMount () {
-        fetch(`https://apiv2.apifootball.com/?action=get_countries&APIkey=5f990ce48ebc7388ba775b57240a09e34a74f3ac19ce1d51ee35839a1e2ce4e2`)
-        .then(response => {console.log(response);})
-        //.then(response => response.json())
-        /*.then(response => this.setState({
-            clubs: response
-        }))*/
-    }
+    // componentDidMount () {
+    //     fetch(`https://apiv2.apifootball.com/?action=get_countries&APIkey=5f990ce48ebc7388ba775b57240a09e34a74f3ac19ce1d51ee35839a1e2ce4e2`)
+    //     // .then(response => {console.log(response);})
+    //     .then(response => response.json())
+    //     .then(response => this.setState({
+    //         clubs: response
+    //     }))
+    // }
+
+    
     
     render() {
+        // const { clubs } = this.props;
         return (
             <div>
-                List of clubs
+                {/* <p>{this.state.clubs}</p> */}
+                <p>Lista natjecanja prema državi</p>
+                {/* <ul>
+                    <li>
+                        {
+                            clubs.map(club => <div key={club.country_id}>{club.country_name}</div>)
+                        }
+                    </li>
+                </ul> */}
+                {/* <ul>
+                    {clubs.map(club => (
+                        <li key={club.country_id}>
+                            Name: {club.country_name}
+                        </li>
+                    ))}
+                </ul> */}
             </div>
         )
     }
