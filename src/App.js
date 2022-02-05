65b69610d73b62dbb06a0bd59c91d7eef5068ff1
Marko-batman <krznarm26@gmail.com>
2022-02-05 01:35:55 +0100

    Added header with countries

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import ListClubs from "./components/ListClubs";
+import Header from "./containers/Header/Header";
 import "./style.scss";
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <ListClubs />
       </div>
     );
