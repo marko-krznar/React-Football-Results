6940c96e6c8aae99f08f6931249d19ed61f2c3b1
Marko-batman <krznarm26@gmail.com>
2022-02-07 20:37:43 +0100

    Configuration and creating routes for leagues

--- src/components/ListLeagues/ListLeagues.js
+++ src/components/ListLeagues/ListLeagues.js
@@ -1,23 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { GETapi } from "../../Api";
 
 export default function Header() {
   const [leaguesList, setLeaguesList] = useState([]);
+  const params = useParams();
 
   useEffect(() => {
     GETapi(
-      "https://apiv3.apifootball.com/?action=get_leagues&APIkey=5f990ce48ebc7388ba775b57240a09e34a74f3ac19ce1d51ee35839a1e2ce4e2"
+      `https://apiv3.apifootball.com/?action=get_leagues&country_id=${params.id}&APIkey=5f990ce48ebc7388ba775b57240a09e34a74f3ac19ce1d51ee35839a1e2ce4e2`
     ).then((res) => setLeaguesList(res));
-  }, []);
+  }, [params.id]);
 
   return (
-    <nav className="menu">
+    <div className="Test">
       <p>Leagues</p>
-      <ul>
-        {leaguesList.map((league, index) => (
-          <li key={index}>{league?.league_name}</li>
-        ))}
-      </ul>
-    </nav>
+      {leaguesList.map((league) => (
+        <p key={league.league_id}>
+          <img src={league.league_logo} alt={league.league_name} />
+          {league.league_name}
+        </p>
+      ))}
+    </div>
   );
 }
